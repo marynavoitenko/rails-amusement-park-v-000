daf81821ca41ce711d0ae4d22877c902908e3b30
github username <github email address>
2018-03-15 02:05:07 -0400

    add attraction create controller action for admin

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -13,9 +13,18 @@ class AttractionsController < ApplicationController
   end
 
   def create
-    # create new attraction
-    # attraction_params
-    # helper method filter ???
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      redirect_to new_attraction_path
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 
 end
